c02d0fca629eeaa85154a0efdf6f386e5ed5a1ed
Aryan Arora <82714299+Aryan1612@users.noreply.github.com>
2024-08-22 18:43:04 +0530

    Update objective_solution.cpp

--- objective_solution.cpp
+++ objective_solution.cpp
@@ -2,15 +2,195 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Function prototypes
-vector<vector<int>> getCombinations(vector<int> &arr, int r);
-float error(const vector<float> &x0, const vector<float> &x1);
-void print(const vector<float> &x);
-bool diagonally_dominant(const vector<vector<float>> &a);
-void make_dominant(vector<vector<float>> &a, vector<float> &b);
-bool gauss_seidel(vector<vector<float>> &a, vector<float> &b, vector<float> &solution);
-void printMatrix(const vector<vector<float>> &matrix, vector<float> &subRHS);
-void combinationsUntil(const vector<int> &arr, vector<int> &current, int start, int r, vector<vector<int>> &combinations);
+class LinearProgram {
+private:
+    int n, m; // Number of variables and constraints
+    vector<vector<float>> a; // Coefficients matrix
+    vector<float> b; // Right-hand side constants
+    vector<float> c; // Coefficients of the objective function
+    bool isMaximize; // Objective function type (maximize or minimize)
+
+public:
+    // Constructor to initialize the problem
+    LinearProgram(int n, int m, const vector<vector<float>>& a, const vector<float>& b, const vector<float>& c, bool isMaximize)
+        : n(n), m(m), a(a), b(b), c(c), isMaximize(isMaximize) {}
+
+    // Function to find and print the optimal solution
+    void solve() {
+        vector<int> arr(n);
+        iota(arr.begin(), arr.end(), 0);
+        vector<vector<int>> combinations = getCombinations(arr, m);
+
+        vector<float> optimalSolution(n, 0);
+        float optimalValue = isMaximize ? -FLT_MAX : FLT_MAX;
+
+        cout << fixed << setprecision(5);
+        cout << "List of basic feasible solutions and their objective values:\n";
+
+        for (auto& combination : combinations) {
+            vector<vector<float>> subMatrix(m, vector<float>(m));
+            vector<float> subRHS(m, 0); // Right-hand side for the sub-system
+            vector<float> solution(n, 0);
+
+            // Extract the sub-matrix and sub-RHS for current combination
+            for (int i = 0; i < m; i++) {
+                subRHS[i] = b[i];
+                for (int j = 0; j < m; j++) {
+                    subMatrix[i][j] = a[i][combination[j]];
+                }
+            }
+
+            // Print the sub-matrix
+            cout << "\nSub-matrix for combination ";
+            for (int idx : combination) {
+                cout << idx + 1 << " ";
+            }
+            cout << ":\n";
+            printMatrix(subMatrix, subRHS);
+
+            // Make the matrix diagonally dominant
+            makeDominant(subMatrix, subRHS);
+
+            // Solve using Gauss-Seidel method
+            if (gaussSeidel(subMatrix, subRHS, solution)) {
+                // Check if solution is basic feasible
+                print(solution);
+                bool isFeasible = all_of(solution.begin(), solution.end(), [](float val) { return val >= 0; });
+
+                if (isFeasible) {
+                    // Map the solution back to the original variable indices
+                    vector<float> fullSolution(n, 0);
+                    for (int i = 0; i < m; i++) {
+                        fullSolution[combination[i]] = solution[i];
+                    }
+
+                    float objectiveValue = inner_product(fullSolution.begin(), fullSolution.end(), c.begin(), 0.0f);
+
+                    // Print the solution and objective value
+                    cout << "Basic feasible solution: ";
+                    print(fullSolution);
+                    cout << "Objective value: " << objectiveValue << endl;
+
+                    // Update optimal solution and value
+                    if ((isMaximize && objectiveValue > optimalValue) || (!isMaximize && objectiveValue < optimalValue)) {
+                        optimalValue = objectiveValue;
+                        optimalSolution = fullSolution;
+                    }
+                }
+            }
+        }
+
+        // Print the optimal solution and value
+        cout << "\nOptimal solution: ";
+        print(optimalSolution);
+        cout << "Optimal objective value: " << optimalValue << endl;
+    }
+
+private:
+    vector<vector<int>> getCombinations(vector<int>& arr, int r) {
+        vector<vector<int>> combinations;
+        vector<int> current;
+        combinationsUntil(arr, current, 0, r, combinations);
+        return combinations;
+    }
+
+    void combinationsUntil(const vector<int>& arr, vector<int>& current, int start, int r, vector<vector<int>>& combinations) {
+        if (current.size() == r) {
+            combinations.push_back(current);
+            return;
+        }
+        for (int i = start; i < (int)arr.size(); i++) {
+            current.push_back(arr[i]);
+            combinationsUntil(arr, current, i + 1, r, combinations);
+            current.pop_back();
+        }
+    }
+
+    bool diagonallyDominant(const vector<vector<float>>& a) {
+        for (int i = 0; i < a.size(); i++) {
+            float sum = accumulate(a[i].begin(), a[i].end(), 0.0f, [&](float acc, float val) { return acc + abs(val); });
+            if (sum - abs(a[i][i]) >= abs(a[i][i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    float calculateError(const vector<float>& x0, const vector<float>& x1) {
+        float err = 0;
+        for (int i = 0; i < x0.size(); i++) {
+            err += pow((x0[i] - x1[i]), 2);
+        }
+        return sqrt(err);
+    }
+
+    void makeDominant(vector<vector<float>>& a, vector<float>& b) {
+        vector<int> indices(a.size());
+        for (int i = 0; i < a.size(); i++) {
+            int sum = accumulate(a[i].begin(), a[i].end(), 0.0f, [](float acc, float val) { return acc + abs(val); });
+            for (int j = 0; j < a[i].size(); j++) {
+                if (abs(a[i][j]) > sum - abs(a[i][j])) {
+                    indices[i] = j;
+                }
+            }
+        }
+        set<int> st{indices.begin(), indices.end()};
+        if (st.size() < indices.size()) {
+            cout << "Not diagonally dominant in any case, Gauss-Seidel may not converge\n";
+        } else {
+            vector<vector<float>> temp(a.size(), vector<float>(a[0].size(), 0));
+            vector<float> c(a.size(), 0);
+            for (int i = 0; i < a.size(); i++) {
+                temp[i] = a[indices[i]];
+                c[i] = b[indices[i]];
+            }
+            a = temp;
+            b = c;
+        }
+    }
+
+    void print(const vector<float>& x) {
+        for (int i = 0; i < x.size(); i++) {
+            cout << "x_" << i + 1 << " = " << x[i] << " ";
+        }
+        cout << "\n";
+    }
+
+    void printMatrix(const vector<vector<float>>& matrix, vector<float>& subRHS) {
+        for (int i = 0; i < matrix.size(); i++) {
+            for (float val : matrix[i]) {
+                cout << setw(10) << val << " ";
+            }
+            cout << " | " << subRHS[i] << endl;
+        }
+    }
+
+    bool gaussSeidel(vector<vector<float>>& a, vector<float>& b, vector<float>& solution) {
+        int n = a.size();
+        vector<float> x(n, 0);
+        cout << "Diagonally dominant (0 or 1): " << diagonallyDominant(a) << "\n";
+
+        if (diagonallyDominant(a)) {
+            vector<float> prev_x(n, 0);
+            do {
+                prev_x = x;
+                for (int j = 0; j < n; j++) {
+                    float sum = b[j];
+                    for (int k = 0; k < n; k++) {
+                        if (k != j) {
+                            sum -= a[j][k] * x[k];
+                        }
+                    }
+                    x[j] = sum / a[j][j];
+                }
+            } while (calculateError(x, prev_x) > 0.000001);
+
+            solution = x;
+            return true;
+        }
+        return false;
+    }
+};
 
 int main() {
     int n, m;
@@ -39,206 +219,8 @@ int main() {
     cin >> opt;
     bool isMaximize = (opt == "max");
 
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++)
-        arr[i] = i;
-    vector<vector<int>> combinations = getCombinations(arr, m);
-
-    vector<float> optimalSolution(n, 0);
-    float optimalValue = isMaximize ? -FLT_MAX : FLT_MAX;
-
-    cout << fixed << setprecision(5);
-    cout << "List of basic feasible solutions and their objective values:\n";
-
-    for (auto combination : combinations) {
-        vector<vector<float>> subMatrix(m, vector<float>(m));
-        vector<float> subRHS(m, 0); // Right-hand side for the sub-system
-        vector<float> solution(n, 0);
-
-        // Extract the sub-matrix and sub-RHS for current combination
-        for (int i = 0; i < m; i++) {
-            subRHS[i] = b[i];
-            for (int j = 0; j < m; j++) {
-                subMatrix[i][j] = a[i][combination[j]];
-            }
-        }
-
-        // Print the sub-matrix
-        cout << "\nSub-matrix for combination ";
-        for (int idx : combination) {
-            cout << idx + 1 << " ";
-        }
-        cout << ":\n";
-        printMatrix(subMatrix, subRHS);
-
-        // Make the matrix diagonally dominant
-        make_dominant(subMatrix, subRHS);
-
-        // Solve using Gauss-Seidel method
-        if (gauss_seidel(subMatrix, subRHS, solution)) {
-            // Check if solution is basic feasible
-            print(solution);
-            bool isFeasible = true;
-            for (float val : solution) {
-                if (val < 0) {
-                    isFeasible = false;
-                    break;
-                }
-            }
-
-            if (isFeasible) {
-                // Map the solution back to the original variable indices
-                vector<float> fullSolution(n, 0);
-                for (int i = 0; i < m; i++) {
-                    fullSolution[combination[i]] = solution[i];
-                }
-
-                float objectiveValue = 0;
-                for (int i = 0; i < n; i++) {
-                    objectiveValue += fullSolution[i] * c[i];
-                }
-
-                // Print the solution and objective value
-                cout << "Basic feasible solution: ";
-                print(fullSolution);
-                cout << "Objective value: " << objectiveValue << endl;
-
-                // Update optimal solution and value
-                if ((isMaximize && objectiveValue > optimalValue) ||
-                    (!isMaximize && objectiveValue < optimalValue)) {
-                    optimalValue = objectiveValue;
-                    optimalSolution = fullSolution;
-                }
-            }
-        }
-    }
-
-    // Print the optimal solution and value
-    cout << "\nOptimal solution: ";
-    print(optimalSolution);
-    cout << "Optimal objective value: " << optimalValue << endl;
+    LinearProgram lp(n, m, a, b, c, isMaximize);
+    lp.solve();
 
     return 0;
-}
-
-vector<vector<int>> getCombinations(vector<int> &arr, int r) {
-    vector<vector<int>> combinations;
-    vector<int> current;
-    combinationsUntil(arr, current, 0, r, combinations);
-    return combinations;
-}
-
-void combinationsUntil(const vector<int> &arr, vector<int> &current, int start, int r, vector<vector<int>> &combinations) {
-    int curr_size = current.size();
-    if (curr_size == r) {
-        combinations.push_back(current);
-        return;
-    }
-    for (int i = start; i < (int)arr.size(); i++) {
-        current.push_back(arr[i]);
-        combinationsUntil(arr, current, i + 1, r, combinations);
-        current.pop_back();
-    }
-}
-
-bool diagonally_dominant(const vector<vector<float>> &a) {
-    int cols = a[0].size();
-    int rows = a.size();
-    for (int i = 0; i < rows; i++) {
-        float sum = 0;
-        for (int j = 0; j < cols; j++) {
-            sum += abs(a[i][j]);
-        }
-        if (sum - abs(a[i][i]) >= abs(a[i][i]))
-            return false;
-    }
-    return true;
-}
-
-float error(const vector<float> &x0, const vector<float> &x1) {
-    float err = 0;
-    int n = x0.size();
-    for (int i = 0; i < n; i++) {
-        err += pow((x0[i] - x1[i]), 2);
-    }
-    err = sqrt(err);
-    return err;
-}
-
-void make_dominant(vector<vector<float>> &a, vector<float> &b) {
-    int rows = a.size();
-    int cols = a[0].size();
-    vector<int> indices(rows);
-    for (int i = 0; i < rows; i++) {
-        int sum = 0;
-        for (int j = 0; j < cols; j++)
-            sum += abs(a[i][j]);
-        for (int j = 0; j < cols; j++) {
-            if (abs(a[i][j]) > sum - abs(a[i][j])) {
-                indices[i] = j;
-            }
-        }
-    }
-    set<int> st{indices.begin(), indices.end()};
-    if ((int)st.size() < (int)indices.size())
-        cout << "Not diagonally dominant in any case, Gauss-Seidel may not converge\n";
-    else {
-        vector<vector<float>> temp(rows, vector<float>(cols, 0));
-        vector<float> c(rows, 0);
-        for (int i = 0; i < rows; i++) {
-            temp[i] = a[indices[i]];
-            c[i] = b[indices[i]];
-        }
-        a = temp;
-        b = c;
-    }
-}
-
-void print(const vector<float> &x) {
-    int n = x.size();
-    for (int i = 0; i < n; i++) {
-        cout << "x_" << i + 1 << " = " << x[i] << " ";
-    }
-    cout << "\n";
-    return;
-}
-
-void printMatrix(const vector<vector<float>> &matrix, vector<float> &subRHS) {
-    int i{0};
-    for (const auto &row : matrix) {
-        for (float val : row) {
-            cout << setw(10) << val << " ";
-        }
-        cout << " | " << subRHS[i++];
-        cout << endl;
-    }
-}
-
-bool gauss_seidel(vector<vector<float>> &a, vector<float> &b, vector<float> &solution) {
-    int n = a.size();
-    vector<float> x(n, 0);
-    cout << "Diagonally dominant (0 or 1): " << diagonally_dominant(a) << "\n";
-
-    if (diagonally_dominant(a)) {
-        vector<float> prev_x(n, 0);  // Previous values of x
-        do {
-            prev_x = x;  // Update prev_x to the current values of x
-
-            for (int j = 0; j < n; j++) {
-                float sum = b[j];
-                for (int k = 0; k < n; k++) {
-                    if (k != j) {
-                        sum -= a[j][k] * x[k];
-                    }
-                }
-                x[j] = sum / a[j][j];
-            }
-
-        } while (abs(error(x, prev_x)) > 0.000001);
-
-        solution = x;
-        return true;
-    } else {
-        return false;
-    }
 }
